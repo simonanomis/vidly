071f6f96290253697c4dea85cbc85e8a52f8f14c
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-06-01 14:00:06 +0200

    Registering Users

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
+const users = require('./routes/users');
 const rentals = require('./routes/rentals');
 const express = require('express');
 const app = express();
@@ -17,6 +18,7 @@ app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
