1d0dcacaddcded334697cddbd31c5a798dc6cbea
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-06-02 12:17:34 +0200

    Setting response headers

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+const config = require('config');
+const jwt = require('jsonwebtoken');
 const _= require('lodash');
 const bcrypt = require('bcrypt');
 const {User, validate} = require('../models/user');
@@ -17,7 +19,9 @@ router.post('/', async (req, res) => {
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
  
-  res.send( _.pick(user, ['_id', 'name', 'email']));
+  const token = jwt.sign({_id: user._id}, config.get('jwtPrivateKey'));
+
+  res.header('x-auth-token', token).send( _.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router; 
