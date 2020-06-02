2ddb566838762a3d9fcdec27d602f4da59fb8c26
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-06-02 11:36:21 +0200

    Storing secrets in env vars

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
@@ -9,6 +10,11 @@ const rentals = require('./routes/rentals');
 const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
+
+if(!config.get('jwtPrivateKey')) {
+  console.error('FATAL ERROR: jwtPrivateKey is not defined');
+  process.exit(1);
+}
 
 mongoose.connect('mongodb://localhost/vidly')
   .then(() => console.log('Connected to MongoDB...'))
