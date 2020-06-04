5462b73df0fbe11756541cdfe13d53d718d6fa14
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-06-04 12:28:42 +0200

    add express async errors

--- routes/genres.js
+++ routes/genres.js
@@ -1,4 +1,3 @@
-const asyncMiddleware = require('../middleware/async');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 const {Genre, validate} = require('../models/genre');
@@ -7,12 +6,12 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/', asyncMiddleware(async (req, res) => {
+router.get('/', async (req, res) => {
     const genres = await Genre.find().sort('name');
     res.send(genres);
-}));
+});
 
-router.post('/', auth, asyncMiddleware(async (req, res) => {
+router.post('/', auth, async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -20,9 +19,9 @@ router.post('/', auth, asyncMiddleware(async (req, res) => {
   genre = await genre.save();
   
   res.send(genre);
-}));
+});
 
-router.put('/:id', asyncMiddleware(async (req, res) => {
+router.put('/:id', async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -33,22 +32,22 @@ router.put('/:id', asyncMiddleware(async (req, res) => {
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
   
   res.send(genre);
-}));
+});
 
-router.delete('/:id', [auth, admin], asyncMiddleware(async (req, res) => {
+router.delete('/:id', [auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
   res.send(genre);
-}));
+});
 
-router.get('/:id', asyncMiddleware(async (req, res) => {
+router.get('/:id', async (req, res) => {
   const genre = await Genre.findById(req.params.id);
 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
   res.send(genre);
-}));
+});
 
 module.exports = router;
