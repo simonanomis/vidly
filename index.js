5b0a4c242a27970b27aebc8ce3a1f8648bfed57c
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-06-04 12:43:51 +0200

    logging errors using Winston Silver :)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-require('express-async-errors')
-const error = require('./middleware/error')
+require('express-async-errors');
+const winston = require('winston');
+const error = require('./middleware/error');
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
@@ -12,6 +13,8 @@ const rentals = require('./routes/rentals');
 const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
+
+winston.add(new winston.transports.File({ filename: 'logfile.log' }));
 
 if(!config.get('jwtPrivateKey')) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined');
