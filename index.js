49f9e630f1f9bd97815d780abad0dd48a532bf21
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-06-30 12:53:12 +0200

    extracting the config logic

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
@@ -7,15 +6,7 @@ const app = express();
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
-
-
-const p = Promise.reject(new Error('Something failed'));
-p.then(() => console.log('Done'));
-
-if(!config.get('jwtPrivateKey')) {
-  console.error('FATAL ERROR: jwtPrivateKey is not defined');
-  process.exit(1);
-}
+require('./startup/config')();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
