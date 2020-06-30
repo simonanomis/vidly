1c8fdfdf2901ab36eac02f3ea5854e78e5515d48
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-06-30 12:49:16 +0200

    extracting the logging logigic

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
-require('express-async-errors');
-const winston = require('winston');
-require('winston-mongodb').MongoDB;
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
 const app = express();
+
+require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 
-winston.exceptions.handle(
-  new winston.transports.File({filename: 'uncaughtExections.log'}));
-
-process.on('unhandledRejection', ex => {
-  throw ex;
-});
-
-winston.add(new winston.transports.File({ filename: 'logfile.log' }));
-winston.add(new winston.transports.MongoDB, {
-  db: 'localhost/vidly', 
-  level: 'info'
-});
 
 const p = Promise.reject(new Error('Something failed'));
 p.then(() => console.log('Done'));
---
+++ startup/logging.js
@@ -0,0 +1,18 @@
+const winston = require('winston');
+require('winston-mongodb');
+require('express-async-errors');
+
+module.exports = function() {
+    winston.exceptions.handle(
+        new winston.transports.File({filename: 'uncaughtExections.log'}));
+      
+      process.on('unhandledRejection', ex => {
+        throw ex;
+      });
+      
+      winston.add(new winston.transports.File({ filename: 'logfile.log' }));
+      winston.add(new winston.transports.MongoDB, {
+        db: 'mongodb://localhost/vidly', 
+        level: 'info'
+      });
+}
