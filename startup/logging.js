73d59c801da0d34b4a63e6c61ce41cb36deb926f
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-06-30 13:00:52 +0200

    colorize exceptions in console

--- startup/logging.js
+++ startup/logging.js
@@ -4,6 +4,7 @@ require('express-async-errors');
 
 module.exports = function() {
     winston.exceptions.handle(
+        new winston.transports.Console({ colorize: true, prettyPrint: true }),
         new winston.transports.File({filename: 'uncaughtExections.log'}));
       
       process.on('unhandledRejection', ex => {
