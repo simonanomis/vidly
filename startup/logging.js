36ac1ccaa27c678e630bb1cef9bca4885ca9bf10
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-08-07 13:28:49 +0200

    setting up the test db

--- startup/logging.js
+++ startup/logging.js
@@ -1,5 +1,5 @@
 const winston = require('winston');
-require('winston-mongodb');
+//require('winston-mongodb');
 require('express-async-errors');
 
 module.exports = function() {
@@ -12,8 +12,8 @@ module.exports = function() {
       });
       
       winston.add(new winston.transports.File({ filename: 'logfile.log' }));
-      winston.add(new winston.transports.MongoDB, {
-        db: 'mongodb://localhost/vidly', 
-        level: 'info'
-      });
+     // winston.add(new winston.transports.MongoDB, {
+     //   db: 'mongodb://localhost/vidly', 
+    //    level: 'info'
+   //   });
 }
