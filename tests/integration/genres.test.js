ab9d4f2239815c2b7d11f70ca551be2acd4f14dd
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-08-10 15:17:09 +0200

    populate the test db

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,13 +1,25 @@
 const request = require('supertest');
+const {Genre} = require('../../models/genre');
 let server;
 
 describe('/api/genres', () => {
     beforeEach(() => { server = require('../../index'); });
-    afterEach(() => { server.close(); });
+    afterEach(async () => { 
+        server.close();
+        await Genre.remove({}); 
+    });
+
     describe('GET /', () => {
         it('should return all genres', async () => {
+            await Genre.collection.insertMany([
+                {name: 'genre1'},
+                {name: 'genre2'},
+            ]);
             const response = await request(server).get('/api/genres');
             expect(response.status).toBe(200);
+            expect(response.body.length).toBe(2);
+            expect(response.body.some(genre => genre.name === 'genre1')).toBeTruthy();
+            expect(response.body.some(genre => genre.name === 'genre2')).toBeTruthy();
         });
     });
 });
