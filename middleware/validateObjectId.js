7db54803ea1dcadb2b0fbc16baaf21b9bb010686
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-08-10 15:41:45 +0200

    refactor with adding validateObjectId

---
+++ middleware/validateObjectId.js
@@ -0,0 +1,9 @@
+const { Mongoose } = require("mongoose")
+const mongoose = require('mongoose');
+
+module.exports = function(req, res, next) {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id))
+        return res.status(404).send('Invalid ID'); 
+    
+    next();
+}
--- routes/genres.js
+++ routes/genres.js
@@ -1,3 +1,4 @@
+const validateObjectId = require('../middleware/validateObjectId');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 const {Genre, validate} = require('../models/genre');
@@ -43,10 +44,7 @@ router.delete('/:id', [auth, admin], async (req, res) => {
   res.send(genre);
 });
 
-router.get('/:id', async (req, res) => {
-  if (!mongoose.Types.ObjectId.isValid(req.params.id))
-    return res.status(404).send('Invalid ID');
-    
+router.get('/:id', validateObjectId, async (req, res) => {
   const genre = await Genre.findById(req.params.id);
 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
