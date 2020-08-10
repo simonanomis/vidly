695f09a5fec44af6865bd4c3e283519f3c421c14
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-08-10 16:00:25 +0200

    POST: return 400 if genre is less than 5 chanrs or more than 50 chars

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,5 +1,7 @@
 const request = require('supertest');
 const {Genre} = require('../../models/genre');
+const {User} = require('../../models/user');
+const mongoose = require('mongoose');
 let server;
 
 describe('/api/genres', () => {
@@ -48,6 +50,30 @@ describe('/api/genres', () => {
                 name: 'genres1' 
             });
             expect(response.status).toBe(401);
+        });
+
+        it('should return 400 if genre is less than 5 chars', async () => {
+            const token = new User().generateAuthToken();
+
+            const response = await request(server)
+            .post('/api/genres')
+            .set('x-auth-token', token)
+            .send({
+                name: '1234' 
+            });
+            expect(response.status).toBe(400);
+        });
+
+        it('should return 400 if genre is more than 50 chars', async () => {
+            const token = new User().generateAuthToken();
+            const name = new Array(53).join('a'); //generate long string
+            const response = await request(server)
+            .post('/api/genres')
+            .set('x-auth-token', token)
+            .send({
+                name: name 
+            });
+            expect(response.status).toBe(400);
         });
     });
 
